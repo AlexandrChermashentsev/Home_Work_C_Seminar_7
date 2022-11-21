674c9b60dc6c432a6f23d5d03905e628dae3b317
Alexandr Chermashentsev <alexandr.chermashentsev@gmail.com>
2022-11-21 22:50:35 +0500

    add Ex 47

---
+++ Ex_47/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+double[,] InitialDoubleArray(int m, int n)
+{
+    Random random = new Random();
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-99, 100);
+            array[i, j] = array[i, j] / 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = GetNumber("Enter the m - number");
+int n = GetNumber("Enter the n - number");
+
+double[,] array = InitialDoubleArray(m, n);
+PrintArray(array);
